e1b737e86eb6f2a556517c9ec4f06c420e1ea4a5
Gautam G <gxutxm@gmail.com>
2024-02-14 17:52:23 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+from project.ipynb import *
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<h1>hello</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
